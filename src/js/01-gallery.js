84746930ce90b955356a77411a0061c355f223c9
Vladyslav Harshyn <garshyn_98@ukr.net>
2022-05-29 17:46:34 +0300

    1 & 2 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+'use strict';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+
+function addItem() {
+  return galleryItems
+    .map(
+      items =>
+        `<a class="gallery__item" href="${items.original}">
+  <img class="gallery__image" src="${items.preview}" alt="${items.description}" />
+</a>`,
+    )
+    .join('');
+}
+
+gallery.insertAdjacentHTML('afterbegin', addItem(galleryItems));
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', throttle(onTimeOfPlayer, 1000));
+
+function onTimeOfPlayer({ seconds }) {
+  console.log('played the video!');
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+let time = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(time);
