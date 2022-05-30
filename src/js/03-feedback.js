df3c8aa642b4615b67d3ce4209c44641d6a54c56
Vladyslav Harshyn <garshyn_98@ukr.net>
2022-05-30 17:32:30 +0300

    ended 3rd task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,53 @@
+'use strict';
 
+import throttle from 'lodash.throttle';
+
+const CONTACT_FORM_LOCAL_STORAGE_KEY = 'formData';
+const contactFormEl = document.querySelector('.feedback-form');
+const formData = {};
+
+const fillContactForm = form => {
+  const formDataLclEl = JSON.parse(localStorage.getItem(CONTACT_FORM_LOCAL_STORAGE_KEY));
+  const formUnit = form.elements;
+
+  for (const key in formDataLclEl) {
+    if (formDataLclEl.hasOwnProperty(key)) {
+      formUnit[key].value = formDataLclEl[key];
+    }
+  }
+};
+
+fillContactForm(contactFormEl);
+
+const onContactFocusChange = throttle(event => {
+  const { target } = event;
+
+  const formTarget = target.value;
+  const formTargetName = target.name;
+
+  formData[formTargetName] = formTarget;
+  localStorage.setItem('formData', JSON.stringify(formData));
+}, 500);
+
+const submit = event => {
+  event.preventDefault();
+
+  const { email, message } = event.target;
+
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+  const obj = {
+    email: contactFormEl.email.value,
+    message: contactFormEl.message.value,
+  };
+
+  console.log(obj);
+  event.currentTarget.reset();
+  localStorage.removeItem('formData');
+};
+
+contactFormEl.addEventListener('submit', submit);
+
+contactFormEl.addEventListener('input', onContactFocusChange);
+/// ураааааа, я наконец сделал!!!!
